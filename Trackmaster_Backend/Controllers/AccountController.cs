4dc04b1d4c69ec198ccc44bd9574b616a0a41554
Pankaj Nandal <pankaj.nandal@blackboxgps.net>
2026-04-30 10:11:34 +0530

    Commit for structure

---
+++ Trackmaster_Backend/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Trackmaster_Service.Interface;
+namespace Trackmaster_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+    }
+}
---
+++ Trackmaster_Service/Service/AccountService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trackmaster_Repository.Interface;
+using Trackmaster_Service.Interface;
+
+namespace Trackmaster_Service.Repository
+{
+    public class AccountService:IAccountService
+    {
+        private readonly IAccountRepository _accountRepository;
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+    }
+}
