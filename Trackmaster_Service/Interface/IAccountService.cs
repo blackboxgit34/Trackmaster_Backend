0498b953cd87e6b704b5c92942caabd6369d76ea
Pankaj Nandal <pankaj.nandal@blackboxgps.net>
2026-05-01 16:21:50 +0530

    Commit for Login Changes

--- Trackmaster_Service/Interface/IAccountService.cs
+++ Trackmaster_Service/Interface/IAccountService.cs
@@ -1,9 +1,11 @@
 ﻿using HMSCL.Models;
+using Trackmaster_Model;
 
 namespace Trackmaster_Service.Interface
 {
     public interface IAccountService
     {
-        LoginUser AuthorizeUser(string userId, string password);
+        LoginUser AuthorizeUser(string userId, string password, string type);
+        List<MasterList> GetUserBySearching(string search);
     }
 }
