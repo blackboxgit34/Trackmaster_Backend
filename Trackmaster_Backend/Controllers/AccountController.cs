779f895f6efb09f640514984a14ef9402efcb769
Pankaj Nandal <pankaj.nandal@blackboxgps.net>
2026-05-04 16:02:50 +0530

    Commit for forget password backend implementation

--- Trackmaster_Backend/Controllers/AccountController.cs
+++ Trackmaster_Backend/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using Trackmaster_Model;
 using Trackmaster_Service.Interface;
 namespace Trackmaster_Backend.Controllers
 {
@@ -7,9 +10,11 @@ namespace Trackmaster_Backend.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAccountService _accountService;
-        public AccountController(IAccountService accountService)
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AccountController(IAccountService accountService, IHttpClientFactory httpClientFactory)
         {
             _accountService = accountService;
+            _httpClientFactory = httpClientFactory;
         }
         [HttpGet("login")]
         public async Task<IActionResult> AuthorizeUser(string userId, string password, string type)
@@ -61,6 +66,51 @@ namespace Trackmaster_Backend.Controllers
                     error = ex.Message
                 });
             }
+        }
+        [HttpGet("VerifyUser")]
+        public async Task<IActionResult> VerifyUser(string username, string website)
+        {
+            try
+            {
+                var url = $"https://api1.trackmaster.in/api/CommonApi/VerifyUser?username={username}&website={website}";
+
+                var client = _httpClientFactory.CreateClient();
+                var response = await client.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return StatusCode((int)response.StatusCode, "External API failed");
+                }
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                var data = JsonSerializer.Deserialize<UserOtp>(content);
+
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet("VerifyUserOtp")]
+        public async Task<IActionResult> VerifyUserOtp(int custid, string website, string OTP)
+        {
+            UserOtp response = _accountService.VerifyUserOtp(custid, website, OTP);
+            if (response.message != "Failed")
+            {
+                return Ok(response);
+            }
+            else
+            {
+                return Ok(response);
+            }
+        }
+        [HttpGet("OTPChangePassword")]
+        public async Task<IActionResult> OTPChangePassword(string custId, string NewPassword)
+        {
+            var result = _accountService.UpdateOTPAdminPassword(custId, NewPassword);
+            return Ok(result);
         }
     }
 }
