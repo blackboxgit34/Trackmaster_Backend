3616317a6823f61ec26e2aa99ff3e42c910743db
Atharv Singh <atharv.singh@blackboxgps.net>
2026-06-25 10:59:53 +0530

    manage poi

--- Trackmaster_Backend/Controllers/GeofenceController.cs
+++ Trackmaster_Backend/Controllers/GeofenceController.cs
@@ -229,7 +229,7 @@ namespace Trackmaster_Backend.Controllers
             }
         }
         [HttpPost("EditPoi")]
-        public async Task<IActionResult> EditPoi([FromBody] EditPoiRequest request)
+        public async Task<IActionResult> EditPoi([FromQuery] EditPoiRequest request)
         {
             try
             {
